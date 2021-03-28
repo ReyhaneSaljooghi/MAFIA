079be1145b6425d30355e0d92a0c0cb9719adf02
Reyhane <reysalphimo@gmail.com>
2021-03-28 05:59:07 -0700

    First commit

---
+++ Silencer.java
@@ -0,0 +1,8 @@
+public class Silencer extends mafia{
+    public Silencer(String name) {
+        super(name);
+    }
+    public  void makeSilent(Player player){
+        player.isSilent=true;
+    }
+}
